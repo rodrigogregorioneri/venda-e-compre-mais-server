57784f4b4dab854df4f2b220ada3ff833e42e10d
t803927 <rodrigo.mazzatech@tokiomarine.com.br>
2019-01-24 10:26:52 -0200

    Configurações Swagger

--- src/main/java/br/com/comprevenda/neri/SwaggerConfig.java
+++ src/main/java/br/com/comprevenda/neri/SwaggerConfig.java
@@ -1,18 +1,21 @@
 package br.com.comprevenda.neri;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.collect.Lists;
+
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @Configuration
 @EnableSwagger2
@@ -48,5 +51,5 @@ public class SwaggerConfig {
                 .build();
     }
 
-}
+
 }
