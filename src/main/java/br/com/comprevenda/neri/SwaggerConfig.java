a769b90e94bacab052bb2f34358f43ad036ecdac
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2019-01-24 10:18:25 -0200

    Update SwaggerConfig.java

--- src/main/java/br/com/comprevenda/neri/SwaggerConfig.java
+++ src/main/java/br/com/comprevenda/neri/SwaggerConfig.java
@@ -17,28 +17,36 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-          .select()
-          .apis(RequestHandlerSelectors.basePackage("br.com.comprevenda.neri.controller"))
-          .paths(PathSelectors.any())
-          .build()
-          .apiInfo(apiInfo())
-          .securitySchemes(Arrays.asList(apiKey()));
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.comprevenda.neri.controller"))
+                .build()
+                .globalOperationParameters(globalOperationParameters())
+                .apiInfo(apiInfo())
+                .enableUrlTemplating(false);
+    }
+
+    private ArrayList<Parameter> globalOperationParameters() {
+        return Lists.newArrayList(new ParameterBuilder()
+                .name("Authorization")
+                .description("Authorization Token")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build());
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("Venda e Compre Mais")
-                .description("The REST API Venda e Compre Mais.").termsOfServiceUrl("")
-                .contact(new Contact("Rodrigo Neri", "Fullstack Developer", "rodrigogregorioneri@hotmail.com"))
-                .license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-                .version("0.0.1")
+        return new ApiInfoBuilder()
+                .title("Título")
+                .description("Descrição")
+                .version("1.0")
                 .build();
     }
 
-    private ApiKey apiKey() {
-        return new ApiKey("rodrigogregorioneri@hotmail.com", "Authorization", "header");
-      }
+}
 }
